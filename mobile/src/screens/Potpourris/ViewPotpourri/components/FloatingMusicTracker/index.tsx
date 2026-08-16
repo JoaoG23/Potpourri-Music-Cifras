f214495e2a770cbfa90228cc827cc3db879f99ab
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2026-08-16 19:15:35 -0300

    fix: Mudar a opacidade da componente de Status de musica

--- mobile/src/screens/Potpourris/ViewPotpourri/components/FloatingMusicTracker/index.tsx
+++ mobile/src/screens/Potpourris/ViewPotpourri/components/FloatingMusicTracker/index.tsx
@@ -286,7 +286,7 @@ const estilos = StyleSheet.create({
     paddingHorizontal: 12,
     paddingBottom: 8,
     borderWidth: 1,
-    opacity: 0.7,
+    opacity: 0.65,
     borderColor: "rgba(0, 0, 0, 0.08)",
     ...Platform.select({
       ios: {
